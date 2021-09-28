ef2941be85b33dc0608fdb279bdcd732391a5745
morisitakanta <morisitakanta@gmail.com>
2021-09-28 19:54:30 +0900

    seiriseiton

---
+++ src/Dijkstra_origin.py
@@ -0,0 +1,137 @@
+"""
+Dijkstra 2D
+@author: huiming zhou
+"""
+
+# import os
+# import sys
+import math
+import heapq
+import plotting_for_Dijkstra as plotting
+import env_for_Dijkstra as env
+
+class Dijkstra:
+    def __init__(self, s_start, s_goal):
+        self.s_start = s_start
+        self.s_goal = s_goal
+
+        self.Env = env.Env()  # class Env
+
+        self.u_set = self.Env.motions  # feasible input set
+        self.obs = self.Env.obs  # position of obstacles
+
+        self.OPEN = []  # priority queue / OPEN set
+        self.CLOSED = []  # CLOSED set / VISITED order
+        self.PARENT = dict()  # recorded parent
+        self.g = dict()  # cost to come
+
+    def searching(self):
+        """
+        Breadth-first Searching.
+        :return: path, visited order
+        """
+
+        self.PARENT[self.s_start] = self.s_start
+        self.g[self.s_start] = 0
+        self.g[self.s_goal] = math.inf
+        heapq.heappush(self.OPEN,
+                       (0, self.s_start))
+
+        while self.OPEN:
+            _, s = heapq.heappop(self.OPEN)
+            self.CLOSED.append(s)
+
+            if s == self.s_goal:
+                break
+
+            for s_n in self.get_neighbor(s):
+                new_cost = self.g[s] + self.cost(s, s_n)
+
+                if s_n not in self.g:
+                    self.g[s_n] = math.inf
+
+                if new_cost < self.g[s_n]:  # conditions for updating Cost
+                    self.g[s_n] = new_cost
+                    self.PARENT[s_n] = s
+                    heapq.heappush(self.OPEN, (new_cost, s_n))
+
+        return self.extract_path(self.PARENT), self.CLOSED
+
+    def get_neighbor(self, s):
+        """
+        find neighbors of state s that not in obstacles.
+        :param s: state
+        :return: neighbors
+        """
+
+        return [(s[0] + u[0], s[1] + u[1]) for u in self.u_set]
+
+    def cost(self, s_start, s_goal):
+        """
+        Calculate Cost for this motion
+        :param s_start: starting node
+        :param s_goal: end node
+        :return:  Cost for this motion
+        :note: Cost function could be more complicate!
+        """
+
+        if self.is_collision(s_start, s_goal):
+            return math.inf
+
+        return math.hypot(s_goal[0] - s_start[0], s_goal[1] - s_start[1])
+
+    def is_collision(self, s_start, s_end):
+        """
+        check if the line segment (s_start, s_end) is collision.
+        :param s_start: start node
+        :param s_end: end node
+        :return: True: is collision / False: not collision
+        """
+
+        if s_start in self.obs or s_end in self.obs:
+            return True
+
+        if s_start[0] != s_end[0] and s_start[1] != s_end[1]:
+            if s_end[0] - s_start[0] == s_start[1] - s_end[1]:
+                s1 = (min(s_start[0], s_end[0]), min(s_start[1], s_end[1]))
+                s2 = (max(s_start[0], s_end[0]), max(s_start[1], s_end[1]))
+            else:
+                s1 = (min(s_start[0], s_end[0]), max(s_start[1], s_end[1]))
+                s2 = (max(s_start[0], s_end[0]), min(s_start[1], s_end[1]))
+
+            if s1 in self.obs or s2 in self.obs:
+                return True
+
+        return False
+
+    def extract_path(self, PARENT):
+        """
+        Extract the path based on the PARENT set.
+        :return: The planning path
+        """
+
+        path = [self.s_goal]
+        s = self.s_goal
+
+        while True:
+            s = PARENT[s]
+            path.append(s)
+
+            if s == self.s_start:
+                break
+
+        return list(path)
+
+def main():
+    s_start = (5, 5)
+    s_goal = (45, 25)
+
+    dijkstra = Dijkstra(s_start, s_goal,)
+    plot = plotting.Plotting(s_start, s_goal)
+
+    path, visited = dijkstra.searching()
+    plot.animation(path, visited, "Dijkstra's")  # animation generate
+
+
+if __name__ == '__main__':
+    main()
