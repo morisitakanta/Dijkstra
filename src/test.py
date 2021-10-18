f8b8771018b0b95e3ded83dd40e260df348d5f32
morisitakanta <morisitakanta@gmail.com>
2021-10-18 16:54:44 +0900

    test

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import manager
 
 def main():
-    print("test")
+    # print("test")
     sim = manager.SimManager(person_num=3, robot_num=3, sim_time_max=10000, sim_time_step=1.0, real_time_simulate=False)
     sim.process()
 
